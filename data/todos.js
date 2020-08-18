5eb919b6e9e417a0ada08d21419a4fb346666e09
Patrick Wilson <pmwilson333@gmail.com>
2020-08-18 15:36:00 -0700

    tables loading with default data

---
+++ data/todos.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    todo: 'Mow the Lawn',
+    completed: false,
+  },
+  {
+    todo: 'Water the Garden',
+    completed: false,
+  },
+  {
+    todo: 'Wash the Car',
+    completed: false,
+  },
+  {
+    todo: 'Clean the Living Room',
+    completed: false,
+  },
+  {
+    todo: 'Laundry',
+    completed: false,
+  }
+];
